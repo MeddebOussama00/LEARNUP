130b3615167702b880397fb33e34b2c33e130f19
MeddebOussama00 <157499891+MeddebOussama00@users.noreply.github.com>
2024-03-22 03:35:13 +0100

    VFE

--- FE/learnUp/src/app/pages/search/search.component.ts
+++ FE/learnUp/src/app/pages/search/search.component.ts
@@ -1,11 +1,12 @@
 import { DOCUMENT } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent  {
+
 
 }
